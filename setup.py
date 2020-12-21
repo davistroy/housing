1872467fbb9532e09acfc05576d83ef2890fc42a
Troy Davis <troy.davis@hotmail.com>
2020-12-21 12:28:05 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Housing analysis from Hands-on Machine Learning book',
+    author='Troy Davis',
+    license='MIT',
+)
